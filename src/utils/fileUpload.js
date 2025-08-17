0aa984c09a2bda251e30c6d0d8ca37014f86fb17
speedy-hussu <speedyhussu.work@gmail.com>
2025-08-17 14:18:56 +0530

    cloudinary file upload

---
+++ src/utils/fileUpload.js
@@ -0,0 +1,22 @@
+import { v2 } from "cloudinary";
+import fs from "fs";
+v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_APIKEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFile = async (filePath) => {
+  try {
+    if (!filePath) return null;
+    const response = await v2.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded successfully", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(filePath); // Delete the file if upload fails
+    return null;
+  }
+};
