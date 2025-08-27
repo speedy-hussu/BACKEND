a55f405278cf0336c0cc2cdc71577f4b390464c0
speedy-hussu <speedyhussu.work@gmail.com>
2025-08-27 17:56:11 +0530

    chane spelling

--- src/utils/index.utils.js
+++ src/utils/index.utils.js
@@ -1,6 +1,6 @@
-import { ApiError } from "./ApiError.js";
+import { ApiError } from "./apiError.js";
 import { asyncHandler } from "./asyncHandler.js";
-import { ApiResponse } from "./ApiResponse.js";
+import { ApiResponse } from "./apiResponse.js";
 import { uploadFile } from "./fileUpload.js";
 
 export { ApiError, asyncHandler, ApiResponse, uploadFile };
