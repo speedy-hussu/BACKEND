acb0b3d719d224f5efc0a0bba9b72e4534cd7a17
speedy-hussu <speedyhussu.work@gmail.com>
2025-08-31 00:00:40 +0530

    Add Subscription model with schema for subscribers and channels

---
+++ src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+const SubscriptionSchema = new Schema(
+  {
+    subscribers: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", SubscriptionSchema);
